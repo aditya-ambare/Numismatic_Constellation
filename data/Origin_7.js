0bcc5552df53810f8e5562b505fa42692f6cff4e
aditya-ambare <adityaambare.official@gmail.com>
2025-09-07 09:50:16 +0100

    back1

--- data/Origin_7.js
+++ data/Origin_7.js
@@ -1,1 +1,1 @@
-var json_Origin_7 = {"type":"FeatureCollection","name":"Origin_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
+var json_Origin_7 = {"type":"FeatureCollection","name":"Origin_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"Sincerely":"Aditya"},"geometry":{"type":"Point","coordinates":[77.417543484037211,23.182310815063826]}}]}
