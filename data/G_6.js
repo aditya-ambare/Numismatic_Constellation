baca5e3164e85465f563d4b099d48f26cc750bcf
aditya-ambare <adityaambare.official@gmail.com>
2026-03-24 06:49:32 +0000

    Venezula

--- data/G_6.js
+++ data/G_6.js
@@ -3,6 +3,6 @@ var json_G_6 = {
 "name": "G_6",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": null, "Name": "Giovanni" }, "geometry": { "type": "Point", "coordinates": [ 14.757610591312531, 36.857580100336435 ] } }
+
 ]
 }
