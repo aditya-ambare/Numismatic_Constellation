2ac6d0b1c402b626e9ece0c559f75f2402041bd1
aditya-ambare <adityaambare.official@gmail.com>
2026-05-24 22:12:57 +0100

    June

--- data/G_6.js
+++ data/G_6.js
@@ -3,6 +3,6 @@ var json_G_6 = {
 "name": "G_6",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": null, "Name": "Giovanni" }, "geometry": { "type": "Point", "coordinates": [ 14.757610591312531, 36.857580100336435 ] } }
+{ "type": "Feature", "properties": { "City": "Modica", "Country": "Italy" }, "geometry": { "type": "Point", "coordinates": [ 14.7613018, 36.8589209 ] } }
 ]
 }
